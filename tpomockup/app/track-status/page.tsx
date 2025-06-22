ea4c7535ce6df9211f28758e2701dcbf8172ba25
NoNiiEa <chanonben4@gmail.com>
2025-06-23 04:25:06 +0700

    Add Track Status page for users to track report status

--- tpomockup/app/track-status/page.tsx
+++ tpomockup/app/track-status/page.tsx
@@ -1,8 +1,8 @@
 export default function TrackStatusPage() {
     return (
         <div className="min-h-screen bg-white flex flex-col items-center justify-center">
-            <h1 className="text-black">Report page</h1>
-            <p className="text-black">This page will allow users to report the crime.</p>
+            <h1 className="text-black">Track Status</h1>
+            <p className="text-black">This page will allow users to track the status of their reports.</p>
         </div>
     );
 }
