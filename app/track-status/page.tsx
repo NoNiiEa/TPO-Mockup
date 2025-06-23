b918d1c47e2839c3b5be4d622399605ec4a6bed4
NoNiiEa <chanonben4@gmail.com>
2025-06-23 17:35:42 +0700

    Test commit to branch

--- app/track-status/page.tsx
+++ app/track-status/page.tsx
@@ -2,6 +2,7 @@ export default function TrackStatusPage() {
     return (
         <div className="min-h-screen bg-white flex flex-col items-center justify-center">
             <h1 className="text-black">Track Status</h1>
+            <h3 className="text-black">Hello world</h3>
             <p className="text-black">This page will allow users to track the status of their reports.</p>
         </div>
     );
